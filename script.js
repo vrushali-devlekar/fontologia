d1d461d7123d65084fa075a0b6f3d8fadd532a7f
vrushali <vrushalidevlekar12@gmail.com>
2025-11-25 10:03:42 +0530

    design

--- script.js
+++ script.js
@@ -1,1 +1,253 @@
-dsd
+const fonts = [
+  {
+    family: "Inter",
+    url: "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap",
+  },
+  {
+    family: "Roboto",
+    url: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap",
+  },
+  {
+    family: "Poppins",
+    url: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap",
+  },
+  {
+    family: "Montserrat",
+    url: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap",
+  },
+  {
+    family: "Open Sans",
+    url: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap",
+  },
+  {
+    family: "Lato",
+    url: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap",
+  },
+  {
+    family: "Nunito",
+    url: "https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap",
+  },
+  {
+    family: "Oswald",
+    url: "https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap",
+  },
+  {
+    family: "Raleway",
+    url: "https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap",
+  },
+  {
+    family: "Ubuntu",
+    url: "https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap",
+  },
+
+  {
+    family: "Playfair Display",
+    url: "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap",
+  },
+  {
+    family: "Merriweather",
+    url: "https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap",
+  },
+  {
+    family: "Source Sans 3",
+    url: "https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@400;700&display=swap",
+  },
+  {
+    family: "Noto Sans",
+    url: "https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap",
+  },
+  {
+    family: "Noto Serif",
+    url: "https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap",
+  },
+
+  {
+    family: "Manrope",
+    url: "https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap",
+  },
+  {
+    family: "Kanit",
+    url: "https://fonts.googleapis.com/css2?family=Kanit:wght@400;700&display=swap",
+  },
+  {
+    family: "Fira Sans",
+    url: "https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap",
+  },
+  {
+    family: "Work Sans",
+    url: "https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap",
+  },
+  {
+    family: "Rubik",
+    url: "https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap",
+  },
+
+  {
+    family: "Cabin",
+    url: "https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&display=swap",
+  },
+  {
+    family: "Mulish",
+    url: "https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap",
+  },
+  {
+    family: "Quicksand",
+    url: "https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap",
+  },
+  {
+    family: "Karla",
+    url: "https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap",
+  },
+  {
+    family: "Barlow",
+    url: "https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&display=swap",
+  },
+
+  {
+    family: "DM Sans",
+    url: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap",
+  },
+  {
+    family: "DM Serif Display",
+    url: "https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap",
+  },
+  {
+    family: "Space Grotesk",
+    url: "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap",
+  },
+  {
+    family: "Space Mono",
+    url: "https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap",
+  },
+  {
+    family: "Inconsolata",
+    url: "https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&display=swap",
+  },
+
+  {
+    family: "PT Sans",
+    url: "https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap",
+  },
+  {
+    family: "PT Serif",
+    url: "https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&display=swap",
+  },
+  {
+    family: "Josefin Sans",
+    url: "https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap",
+  },
+  {
+    family: "Titillium Web",
+    url: "https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap",
+  },
+  {
+    family: "Bebas Neue",
+    url: "https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap",
+  },
+
+  {
+    family: "Dancing Script",
+    url: "https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap",
+  },
+  {
+    family: "Great Vibes",
+    url: "https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap",
+  },
+  {
+    family: "Abril Fatface",
+    url: "https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap",
+  },
+  {
+    family: "Arvo",
+    url: "https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&display=swap",
+  },
+  {
+    family: "Anton",
+    url: "https://fonts.googleapis.com/css2?family=Anton&display=swap",
+  },
+
+  {
+    family: "Cardo",
+    url: "https://fonts.googleapis.com/css2?family=Cardo:wght@400;700&display=swap",
+  },
+  {
+    family: "Hind",
+    url: "https://fonts.googleapis.com/css2?family=Hind:wght@400;700&display=swap",
+  },
+  {
+    family: "Asap",
+    url: "https://fonts.googleapis.com/css2?family=Asap:wght@400;700&display=swap",
+  },
+  {
+    family: "Teko",
+    url: "https://fonts.googleapis.com/css2?family=Teko:wght@400;700&display=swap",
+  },
+  {
+    family: "Merriweather Sans",
+    url: "https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@400;700&display=swap",
+  },
+
+  {
+    family: "Zilla Slab",
+    url: "https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@400;700&display=swap",
+  },
+  {
+    family: "Heebo",
+    url: "https://fonts.googleapis.com/css2?family=Heebo:wght@400;700&display=swap",
+  },
+  {
+    family: "Chivo",
+    url: "https://fonts.googleapis.com/css2?family=Chivo:wght@400;700&display=swap",
+  },
+  {
+    family: "Sora",
+    url: "https://fonts.googleapis.com/css2?family=Sora:wght@400;700&display=swap",
+  },
+];
+
+function getRandomFonts() {
+  let random = Math.floor(Math.random() * fonts.length);
+  return fonts[random].family;
+}
+getRandomFonts();
+
+function loadFonts(fontfamily) {
+  let link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = `https://fonts.googleapis.com/css2?family=${fontfamily.replace(
+    / /g,
+    "+"
+  )}:wght@300&display=swap`;
+  document.head.appendChild(link);
+}
+function getThreeUniqueFonts() {
+  let copy = [...fonts]; // make a copy
+  copy.sort(() => Math.random() - 0.5); // shuffle array
+
+  return [copy[0].family, copy[1].family, copy[2].family];
+}
+
+function applyFontsEverWhere() {
+  let [font1, font2, font3] = getThreeUniqueFonts();
+
+  loadFonts(font1);
+  loadFonts(font2);
+  loadFonts(font3);
+
+  document.querySelector("#left-font1 h5").style.fontFamily = font1;
+  document.querySelector("#left-font1 h5").textContent = font1;
+  document.querySelector("#h1").style.fontFamily = font1;
+
+  document.querySelector("#left-font2").style.fontFamily = font2;
+  document.querySelector("#left-font2 h5").textContent = font2;
+  document.querySelector("#h2").style.fontFamily = font2;
+
+  document.querySelector("#left-font3").style.fontFamily = font3;
+  document.querySelector("#left-font3 h5").textContent = font3;
+  document.querySelector("#h3").style.fontFamily = font3;
+}
+document
+  .querySelector(".generate button")
+  .addEventListener("click", function () {
+    applyFontsEverWhere();
+  });
